952b1af6c1c01b3f8aa3b57d9c09050c94510d90
Jon Olivet <jon.olivet@externos.bci.cl>
2020-04-02 20:24:34 -0300

    Updates to the Commons.rb file.

--- configurations/Commons.rb
+++ configurations/Commons.rb
@@ -1,8 +1,8 @@
 DEBUG_DOMAINS = [
-  'Commons', 'UIElements', 'Autenticacion', 'Consumo', 'Tarjetas', 'Pagos', 'Transferencias', 'Inversiones', 'Transversal', 'OnOff', 'Cuentas', 'PFM', 'Recargas', 'Entrust', 'Fidelizacion', 'Descuentos', 'TarjetaCreditoAvisoViaje', 'TarjetaVirtual', 'AlcanciaDigital'
+  'BasicCommons'
 ]
 
-IGNORE_ASSETS_DOMAINS = [ 'Commons', 'UIElements' ]
+IGNORE_ASSETS_DOMAINS = [ 'BasicCommons']
 
 # Print text to console with this codes: Colors red = 31 green = 32 blue = 34
 def color(color=32)
